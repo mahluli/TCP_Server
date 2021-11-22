b53bda57561a827cebfc95e49f2b9795dfb2a4bb
mahluli <42044971+mahluli@users.noreply.github.com>
2021-11-22 13:10:54 +0200

    Add files via upload

---
+++ webserver/src/webserver.py
@@ -0,0 +1,24 @@
+from flask import Flask, send_file, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    return render_template('index.html')
+
+@app.route('/SensorStream')
+def sensor_stream():
+    #TODO: Add code that displays the contents of log file /data/sensorlog.txt
+    return "I'm suppose to display file contents"
+    
+@app.route('/download')
+def download_file():
+    #TODO: Add code to download the file /data.sensorlog.txt
+    path = "Readings.txt"
+    return send_file(path, as_attachment=True)
+
+#TODO Add the remaining functions requested either by adding more pages to the template or get fancy with more templates and better formatting
+
+if __name__ == '__main__':
+    #app.run(host='0.0.0.0', port=5080, debug=True) #Use this line to test basic functionality locally before trying to deploy on Pi
+    app.run(host='0.0.0.0', port=80)
